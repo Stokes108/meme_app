84a0245fdc7153839e02078cc5358a8668bba184
Abe Stokes <stokes.abraham12345@gmail.com>
2023-05-17 20:02:24 -0400

    changing init file around

--- meme_app/__init__.py
+++ meme_app/__init__.py
@@ -6,10 +6,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db as root_db, login_manager, ma
 from flask_cors import CORS
+from .auth.routes import auth
+
 
 
 app = Flask(__name__)
+
+from helpers import JSONEncoder
+
 CORS(app)
+
+app.register_blueprint(auth)
 
 
 
@@ -20,22 +27,18 @@ login_manager.init_app(app)
 
 ma.init_app(app)
 migrate = Migrate(app, root_db)
-
-
-
-
-
-from helpers import JSONEncoder
 app.json_encoder = JSONEncoder
 
 
+
+
+
+
+
 from .site.routes import site
-from .auth.routes import auth
 from .api.routes import api
 
 
-app.register_blueprint(auth)
 app.register_blueprint(site)
-
 app.register_blueprint(api)
 
